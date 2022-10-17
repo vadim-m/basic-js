0d03b3e2ae389e5351f7ab2530d5da0e191ed372
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-10-17 23:22:20 +0300

    feat: extended-repeater solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+  const word = String(str);
+  const repeatTimes = arguments[1].repeatTimes ? arguments[1].repeatTimes : 1;
+  const separator = arguments[1].separator ? arguments[1].separator : "+";
+
+  const addition =
+    arguments[1].addition ||
+    arguments[1].addition === false ||
+    arguments[1].addition === null
+      ? String(arguments[1].addition)
+      : "";
+  const additionRepeatTimes = arguments[1].additionRepeatTimes
+    ? arguments[1].additionRepeatTimes
+    : 0;
+  const additionSeparator = arguments[1].additionSeparator
+    ? arguments[1].additionSeparator
+    : "|";
+
+  const resultArr = [];
+  let additionStr = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    const additions = [];
+
+    if (additionRepeatTimes > 0) {
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        additions.push(addition);
+      }
+
+      additionStr = additions.join(additionSeparator);
+    } else {
+      additions.push(addition);
+    }
+    additionStr = additions.join(additionSeparator);
+    resultArr.push(`${word}${additionStr}`);
+  }
+
+  return resultArr.join(separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
